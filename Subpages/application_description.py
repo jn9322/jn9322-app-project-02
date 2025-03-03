82e341f855064e58164445339927d45f802397ac
jn9322 <honza.ne@seznam.cz>
2025-03-03 18:20:14 +0000

    03-Mar-2025 - Archimate diagram works

--- Subpages/application_description.py
+++ Subpages/application_description.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.write("ArchiMate diagram:")
+st.write("### ArchiMate diagram:")
+st.write("*For better visibility - put cursor on the picture and click on the icon in the right upper corner")
 st.image("Pictures/Parsing-archimate-diagram.png")
