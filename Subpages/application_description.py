5757e220afd0898da676b5519eb0b4ff75863a9c
jn9322 <honza.ne@seznam.cz>
2025-03-03 18:06:31 +0000

    Test

--- Subpages/application_description.py
+++ Subpages/application_description.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
 st.write("ArchiMate diagram:")
-# st.image("Pictures/Parsing-archimate-diagram.png")
+st.image("Pictures/Parsing-archimate-diagram-MINI.png")
+
+if st.download_button("Download",data = "Pictures/Parsing-archimate-diagram.png"  , file_name="Archimate.jpeg"):
+    st.info("Download will happen in few seconds")
