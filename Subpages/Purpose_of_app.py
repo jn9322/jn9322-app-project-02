7d125355a1e5217331a89df05672a4d91fd30b51
jn9322 <honza.ne@seznam.cz>
2025-03-04 16:49:39 +0000

    04-Mar-2025 test

--- Subpages/Purpose_of_app.py
+++ Subpages/Purpose_of_app.py
@@ -11,6 +11,13 @@ st.write(
 
 st.write(" ")
 
+st.write("##### Business scenario:") 
+st.write(
+    "Simplified version of case when a company does a purchase from other company/e-shop on frequent bases. That is why there is XML invoice defined for invoicing.This parsing application could be seen like very small ERP system/ERP program :)."
+    )
+
+st.write(" ")
+
 st.write("##### Process:") 
 st.write("""
     1) Dowload XML (keep existing or you can update it) 
